28fdc45656d9c5a9203f13ad54ba161b100f9b5f
agl5702 <advasquez41@misena.edu.co>
2024-03-18 14:52:30 -0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import img from './assets/fondo.jpg'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
