2e2491703d85ec342398edcda35f5b20d67160ad
agl5702 <advasquez41@misena.edu.co>
2024-03-18 14:49:23 -0500

    commit

--- src/api/tasks.api.js
+++ src/api/tasks.api.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const URL =
-  process.env.NODE_ENV === "production"
-    ? import.meta.env.VITE_BACKEND_URL
-    : "http://localhost:8000";
+const URL ="http://localhost:8000";
 
 console.log(URL);
 const tasksApi = axios.create({
